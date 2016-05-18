f95d543fc49445b9988d30d6f54d8310679c7b05
yasukuni morishima <linust@hotmail.co.jp>
2016-05-18 12:02:00 +0900

    add overload

--- ARMVisualizer/HTMLVisualizer/ARMResources.cs
+++ ARMVisualizer/HTMLVisualizer/ARMResources.cs
@@ -38,6 +38,8 @@ namespace HTMLVisualizer
         public bool lbpresent;
         public bool avsetpresent;
 
+        public String property1;
+
         public static ARMResources[] PositionCaluculater(ARMResources[] res)
         {
 //            ARMResources restemp1 = res.OrderBy(s => s.level);
@@ -73,6 +75,36 @@ namespace HTMLVisualizer
                 level = -1;
             }
         }
+
+        public ARMResources(String name, int type, String parent, String prop)
+        {
+            x = 0;
+            y = 0;
+            width = 0;
+            height = 0;
+
+            nsgpresent = false;
+            lbpresent = false;
+            avsetpresent = false;
+
+            restype = type;
+            resname = name;
+            resparent = parent;
+
+            property1 = prop;
+
+            reschild = new List<string>();
+
+            if (parent == null)
+            {
+                level = 0;
+            }
+            else
+            {
+                level = -1;
+            }
+        }
+
     }
-    
+
 }
